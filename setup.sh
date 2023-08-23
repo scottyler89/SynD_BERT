3b98a50b1d6c99fd5ff34e8ef5332f3abf2faef7
Scott Tyler <scottyler89@gmail.com>
2023-08-23 14:43:21 -0400

    Create setup.sh
    Set up the colab environment

---
+++ setup.sh
@@ -0,0 +1,21 @@
+pip install triton==2.0.0.dev20221030
+pip install einops
+pip install transformers==4.29.2
+pip install triton
+pip install peft
+pip install omegaconf
+pip install torch
+pip install evaluate
+pip install accelerate
+pip install natsort
+pip install grpcio>=1.48.2
+pip install protobuf>=3.19.6
+pip install werkzeug>=1.0.1
+pip install rsa<5,>=3.1.4
+pip install pyasn1-modules>=0.2.1
+pip install cachetools<6.0,>=2.0.0
+pip install scipy>1.4
+pip install triton==2.0.0.dev20221030
+
+
+git clone https://github.com/Zhihan1996/DNABERT_2
